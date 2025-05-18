b8aaba3b4b7f2de461010318c12770e48ef1138f
thecodingvivek <thisisvivekchitturi@gmail.com>
2025-05-18 08:20:13 +0530

    updated readme

---
+++ app/test/BlogSerializers.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BlogSerializers = ({}) => {
+  
+  return (
+    <div className=""></div>
+  )
+}
+
+export default BlogSerializers
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -24,6 +24,7 @@ export default async function PostPage({
   const post = await client.fetch<SanityDocument>(POST_QUERY, await params, options);
   // const postImageUrl = post.image
   //   ? urlFor(post.image)?.width(550).height(310).url()
+  // console.log(post.body);
   //   : null;
 
   return (
