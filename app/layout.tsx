db39d09d46ebcf1cce0492878c3f6513b03c5bc6
thecodingvivek <thisisvivekchitturi@gmail.com>
2025-05-25 20:23:29 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { bricole  } from "./fonts";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${bricole.className}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
